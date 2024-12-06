ae8bc6dde38c5e8dd6a3267b1d31b5dc89562f41
CRT-123 <1130894061@qq.com>
2024-12-06 18:45:03 +0800

    laoding

--- src/views/user/userListInfo.vue
+++ src/views/user/userListInfo.vue
@@ -1,128 +1,136 @@
 <template>
   <div class="container">
-    <a-card :loading="loading">
-      <a-typography-title :heading="6"> 用户详情 </a-typography-title>
-      <a-divider />
-      <a-form :model="form" style="padding-right: 120px">
-        <a-row :gutter="16">
-          <a-col :span="12">
-            <a-form-item field="value3" label="用户地址" label-col-flex="">
-              <a-input
-                v-model.trim="form.address"
-                placeholder="please enter..."
-                allow-clear
-              />
-            </a-form-item>
-          </a-col>
-          <a-col :span="12">
-            <a-form-item field="value3" label="用户名" label-col-flex="">
-              <a-input
-                v-model.trim="form.name"
-                placeholder="please enter..."
-                allow-clear
-              />
-            </a-form-item>
-          </a-col>
-          <a-col :span="14">
-            <a-form-item field="value2" label="相关机器人ID" label-col-flex="">
-              <a-input
-                v-model.trim="form.robotId"
-                placeholder="please enter..."
-                allow-clear
-              />
-            </a-form-item>
-          </a-col>
-
-          <a-col :span="8">
-            <a-space>
-              <a-button type="primary" @click="queryUserListData()"
-                >查询</a-button
+    <a-spin :loading="loading" style="width: 100%">
+      <a-card>
+        <a-typography-title :heading="6"> 用户详情 </a-typography-title>
+        <a-divider />
+        <a-form :model="form" style="padding-right: 120px">
+          <a-row :gutter="16">
+            <a-col :span="12">
+              <a-form-item field="value3" label="用户地址" label-col-flex="">
+                <a-input
+                  v-model.trim="form.address"
+                  placeholder="please enter..."
+                  allow-clear
+                />
+              </a-form-item>
+            </a-col>
+            <a-col :span="12">
+              <a-form-item field="value3" label="用户名" label-col-flex="">
+                <a-input
+                  v-model.trim="form.name"
+                  placeholder="please enter..."
+                  allow-clear
+                />
+              </a-form-item>
+            </a-col>
+            <a-col :span="14">
+              <a-form-item
+                field="value2"
+                label="相关机器人ID"
+                label-col-flex=""
               >
+                <a-input
+                  v-model.trim="form.robotId"
+                  placeholder="please enter..."
+                  allow-clear
+                />
+              </a-form-item>
+            </a-col>
 
-              <a-button type="primary" @click="resetClick()">重置筛选</a-button>
-            </a-space>
-          </a-col>
-        </a-row>
-      </a-form>
+            <a-col :span="8">
+              <a-space>
+                <a-button type="primary" @click="queryUserListData()"
+                  >查询</a-button
+                >
 
-      <a-grid :cols="24" :col-gap="16" :row-gap="16" style="margin-top: 16px">
-        <a-grid-item
-          :span="{ xs: 24, sm: 24, md: 24, lg: 24, xl: 24, xxl: 24 }"
-        >
-          <a-table
-            :data="userList"
-            style="margin-top: 20px"
-            :pagination="false"
+                <a-button type="primary" @click="resetClick()"
+                  >重置筛选</a-button
+                >
+              </a-space>
+            </a-col>
+          </a-row>
+        </a-form>
+
+        <a-grid :cols="24" :col-gap="16" :row-gap="16" style="margin-top: 16px">
+          <a-grid-item
+            :span="{ xs: 24, sm: 24, md: 24, lg: 24, xl: 24, xxl: 24 }"
           >
-            <template #columns>
-              <a-table-column title="用户ID" data-index="id"></a-table-column>
+            <a-table
+              :data="userList"
+              style="margin-top: 20px"
+              :pagination="false"
+            >
+              <template #columns>
+                <a-table-column title="用户ID" data-index="id"></a-table-column>
 
-              <a-table-column
-                title="用户名称"
-                data-index="name"
-              ></a-table-column>
+                <a-table-column
+                  title="用户名称"
+                  data-index="name"
+                ></a-table-column>
 
-              <a-table-column
-                title="用户地址"
-                data-index="address"
-              ></a-table-column>
+                <a-table-column
+                  title="用户地址"
+                  data-index="address"
+                ></a-table-column>
 
-              <a-table-column title="用户余额" data-index="amount">
-              </a-table-column>
+                <a-table-column title="用户余额" data-index="amount">
+                </a-table-column>
 
-              <a-table-column title="用户头像" data-index="avatar">
-                <template #cell="{ record }">
-                  <img
-                    :src="record.avatar"
-                    alt="头像"
-                    style="
-                      width: 30px;
-                      height: 30px;
-                      border-radius: 50%;
-                      margin-right: 8px;
-                    "
-                  />
-                </template>
-              </a-table-column>
+                <a-table-column title="用户头像" data-index="avatar">
+                  <template #cell="{ record }">
+                    <img
+                      :src="record.avatar"
+                      alt="头像"
+                      style="
+                        width: 30px;
+                        height: 30px;
+                        border-radius: 50%;
+                        margin-right: 8px;
+                      "
+                    />
+                  </template>
+                </a-table-column>
 
-              <a-table-column
-                title="与机器人对话次数"
-                data-index="chatCount"
-              ></a-table-column>
+                <a-table-column
+                  title="与机器人对话次数"
+                  data-index="chatCount"
+                ></a-table-column>
 
-              <a-table-column
-                title="相关机器人"
-                data-index="robotId"
-              ></a-table-column>
+                <a-table-column
+                  title="相关机器人"
+                  data-index="robotId"
+                ></a-table-column>
 
-              <!--              <a-table-column title="操作">-->
-              <!--                <template #cell>-->
-              <!--                  <div>-->
-              <!--                    <a-button type="primary">修改</a-button>-->
-              <!--                    <a-button type="primary">禁止收益</a-button>-->
-              <!--                    <a-button type="primary">禁止提现</a-button>-->
-              <!--                    <a-button type="primary">添加订单</a-button>-->
-              <!--                  </div>-->
-              <!--                </template>-->
-              <!--              </a-table-column>-->
-            </template>
-          </a-table>
-          <div style="display: flex; justify-content: flex-end">
-            <a-pagination
-              :total="total"
-              :current="form.pageIndex + 1"
-              :page-size="10"
-              show-total
-              @change="
-                (current) => {
-                  handlePageChange(current);
-                }
-              "
-            ></a-pagination>
-          </div>
-        </a-grid-item>
-      </a-grid>
-    </a-card>
+                <!--              <a-table-column title="操作">-->
+                <!--                <template #cell>-->
+                <!--                  <div>-->
+                <!--                    <a-button type="primary">修改</a-button>-->
+                <!--                    <a-button type="primary">禁止收益</a-button>-->
+                <!--                    <a-button type="primary">禁止提现</a-button>-->
+                <!--                    <a-button type="primary">添加订单</a-button>-->
+                <!--                  </div>-->
+                <!--                </template>-->
+                <!--              </a-table-column>-->
+              </template>
+            </a-table>
+            <div style="display: flex; justify-content: flex-end">
+              <a-pagination
+                :total="total"
+                :current="form.pageIndex + 1"
+                :page-size="10"
+                show-total
+                @change="
+                  (current) => {
+                    handlePageChange(current);
+                  }
+                "
+              ></a-pagination>
+            </div>
+          </a-grid-item>
+        </a-grid>
+      </a-card>
+    </a-spin>
   </div>
 </template>
 
